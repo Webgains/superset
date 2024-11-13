20d99d9f826b4dda9838621fe26ab5e06244a8e3
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2024-11-13 13:13:43 +0300

    refactor(Avatar): Migrate Avatar to Ant Design 5 (#30740)

--- superset-frontend/src/components/FacePile/index.tsx
+++ superset-frontend/src/components/FacePile/index.tsx
@@ -18,14 +18,12 @@
  */
 import {
   getCategoricalSchemeRegistry,
-  styled,
   isFeatureEnabled,
   FeatureFlag,
-  SupersetTheme,
 } from '@superset-ui/core';
 import getOwnerName from 'src/utils/getOwnerName';
 import { Tooltip } from 'src/components/Tooltip';
-import { Avatar } from 'src/components';
+import { Avatar, AvatarGroup } from 'src/components/Avatar';
 import { getRandomColor } from './utils';
 
 interface FacePileProps {
@@ -35,29 +33,9 @@ interface FacePileProps {
 
 const colorList = getCategoricalSchemeRegistry().get()?.colors ?? [];
 
-const customAvatarStyler = (theme: SupersetTheme) => {
-  const size = theme.gridUnit * 8;
-  return `
-  width: ${size}px;
-  height: ${size}px;
-  line-height: ${size}px;
-  font-size: ${theme.typography.sizes.s}px;`;
-};
-
-const StyledAvatar = styled(Avatar)`
-  ${({ theme }) => customAvatarStyler(theme)}
-`;
-
-// to apply styling to the maxCount avatar
-const StyledGroup = styled(Avatar.Group)`
-  .ant-avatar {
-    ${({ theme }) => customAvatarStyler(theme)}
-  }
-`;
-
 export default function FacePile({ users, maxCount = 4 }: FacePileProps) {
   return (
-    <StyledGroup maxCount={maxCount}>
+    <AvatarGroup max={{ count: maxCount }}>
       {users.map(user => {
         const { first_name, last_name, id } = user;
         const name = getOwnerName(user);
@@ -68,7 +46,7 @@ export default function FacePile({ users, maxCount = 4 }: FacePileProps) {
           : undefined;
         return (
           <Tooltip key={name} title={name} placement="top">
-            <StyledAvatar
+            <Avatar
               key={name}
               style={{
                 backgroundColor: color,
@@ -78,10 +56,10 @@ export default function FacePile({ users, maxCount = 4 }: FacePileProps) {
             >
               {first_name?.[0]?.toLocaleUpperCase()}
               {last_name?.[0]?.toLocaleUpperCase()}
-            </StyledAvatar>
+            </Avatar>
           </Tooltip>
         );
       })}
-    </StyledGroup>
+    </AvatarGroup>
   );
 }
